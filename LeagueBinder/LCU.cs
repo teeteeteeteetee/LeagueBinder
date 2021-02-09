7d18274f090749d8eb7a9db59f3098215e2cac97
lolitee <62479501+lolitee@users.noreply.github.com>
2021-02-09 11:06:20 +0100

    added LCUSharp lib https://github.com/bryanhitc/lcu-sharp

--- LeagueBinder/LCU.cs
+++ LeagueBinder/LCU.cs
@@ -5,10 +5,27 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Net;
 using LCUSharp;
+using System.Diagnostics;
 
 namespace LeagueBinder
 {
     public class LCU
     {
+
+        public Boolean Status { get; set; }
+
+        private async void Connect()
+        {
+
+
+            Status = true;
+
+        }
+
+        public void Load()
+        {
+            Status = false;
+            Connect();
+        }
     }
 }
