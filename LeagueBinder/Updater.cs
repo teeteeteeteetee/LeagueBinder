49f097cb03c2fea702a238eae9c1c8b08e022667
lolitee <62479501+lolitee@users.noreply.github.com>
2021-02-13 22:10:51 +0100

    updater

--- LeagueBinder/Updater.cs
+++ LeagueBinder/Updater.cs
@@ -1,12 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
+using MetroFramework;
 
 namespace LeagueBinder
 {
-    class Updater
+    /// <summary>
+    /// connects to github
+    /// gets list off their api
+    /// </summary>
+    public partial class Updater : MetroFramework.Forms.MetroForm
     {
+        public Updater()
+        {
+            
+        }
     }
 }
